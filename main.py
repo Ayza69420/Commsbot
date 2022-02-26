0143cc71d8b3ae28962cc4be1abbded9b1ce32ee
Ayza <87999011+Ayza69420@users.noreply.github.com>
2022-02-26 12:29:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ intents = discord.Intents(messages=True, guilds=True, reactions=True, members=Tr
 client = commands.Bot(command_prefix = '=', intents = intents)
 
 queue = []
-ids = {}
+sessions = {}
 
 packets = {}
 
@@ -19,10 +19,10 @@ async def on_ready():
 async def call(ctx):
     channel = ctx.channel.id
 
-    if channel not in ids and channel not in queue:
+    if channel not in sessions and channel not in queue:
         await ctx.channel.send("Waiting for an available connection.")
         queue.append(channel)
-    elif channel in ids:
+    elif channel in sessions:
         await ctx.channel.send("You are already in a connection.")
     else:
         await ctx.channel.send("You are already waiting for a connection.")
@@ -32,13 +32,13 @@ async def call(ctx):
 @client.command()
 async def hangup(ctx):
     channel = ctx.channel.id
-    communicating_to = ids[channel]
+    communicating_with = sessions[channel]
 
-    del ids[channel]
-    del ids[communicating_to]
+    del sessions[channel]
+    del sessions[communicating_with]
 
     await client.get_channel(channel).send("Connection has been closed.")
-    await client.get_channel(communicating_to).send("The other party hanged up.")
+    await client.get_channel(communicating_with).send("The other party hanged up.")
 
 async def transmit(channel):
     data = packets[channel]
@@ -53,7 +53,7 @@ async def create_connection():
         await client.get_channel(id1).send("A connection was made!")
         await client.get_channel(id2).send("A connection was made!")
 
-        ids[id1], ids[id2] = id2, id1
+        sessions[id1], sessions[id2] = id2, id1
 
         queue.remove(id1)
         queue.remove(id2)
@@ -63,11 +63,11 @@ async def on_message(ctx):
     channel = ctx.channel.id
     author = ctx.author
 
-    if channel in ids and author.id != client.user.id:
+    if channel in sessions and author.id != client.user.id:
         if ctx.content != "=hangup":
-            packets[ids[channel]] = f"**[{author}]:** {ctx.content}"
+            packets[sessions[channel]] = f"**[{author}]:** {ctx.content}"
         
-            await transmit(ids[channel])
+            await transmit(sessions[channel])
 
     await client.process_commands(ctx)
 
